e6c1f0a2772707de14242f9f0a55dd7dad848c43
yasith <yasith.j@icloud.com>
2025-01-08 14:59:42 -0600

    update tests

--- micro.py
+++ micro.py
@@ -2,38 +2,46 @@
 
 # MICRO BENCHMARKS
 
-import time
-import random
 import argparse
-import rich
+import json
+import random
+import time
+
 import rich.progress
-from sdk import AgentSDK, SSHSDK, SDK
+
+from sdk import SDK, SSHSDK, AgentSDK
+
 
 def test_latency(sdk: SDK, task: str, size_kb: int, reps: int, warmup=0.1) -> float:
     """
     Measure the round-trip latency
     
     """
-    if task == "uload":
+    if task == "UL":
         fn = lambda: sdk.uload(size_kb)
-    elif task == "dload":
+    elif task == "DL":
         fn = lambda: sdk.dload(size_kb)
     latencies = []
     progress = rich.progress.Progress()
     progress.start()
-    prog_bar = progress.add_task("counter", total=reps)
+    prog_bar = progress.add_task(f"Latency ({task}) ⏸", total=reps)
     r = int(reps * warmup)
     counter = 0
+    sdk.setup(size_kb)
     while counter != reps:
         start_time = time.time_ns()
         fn()
         end_time = time.time_ns()
         progress.update(prog_bar, advance=1)
         counter += 1
+        if counter == r:
+            progress.update(prog_bar, description=f"Latency ({task}) ⏵")
         if counter > r:
           latencies.append((end_time - start_time) / 1e9)
-    progress.stop()
     avg_latency = sum(latencies) / len(latencies)
+    sdk.teardown()
+    progress.update(prog_bar, description=f"Latency ({task}): {avg_latency:.6f} s   ")
+    progress.stop()
     return avg_latency
 
 
@@ -42,25 +50,29 @@ def test_thruput(sdk: SDK, task: str, size_kb: int, reps: int, warmup=0.1) -> fl
     Measure the thruput
     
     """
-    if task == "uload":
+    if task == "UL":
         fn = lambda: sdk.uload(size_kb)
-    elif task == "dload":
+    elif task == "DL":
         fn = lambda: sdk.dload(size_kb)
     r = int(reps * warmup)
     counter = 0
     progress = rich.progress.Progress()
     progress.start()
-    prog_bar = progress.add_task("counter", total=reps)
+    prog_bar = progress.add_task(f"Thruput ({task}) ⏸", total=reps)
+    sdk.setup(size_kb)
     while counter != reps:
         if counter == r:
+            progress.update(prog_bar, description=f"Thruput ({task}) ⏵")
             start_time = time.time_ns()
         fn()
         progress.update(prog_bar, advance=1)
         counter += 1
     end_time = time.time_ns()
-    progress.stop()
     total_size_kb = (size_kb * reps)
     thruput = total_size_kb / 1024 / (end_time - start_time) * 1e9
+    sdk.teardown()
+    progress.update(prog_bar, description=f"Thruput ({task}): {thruput:.6f} MB/s")
+    progress.stop()
     return thruput
 
 def test_load(sdk: SDK, size_kb: int, rate: int):
@@ -68,35 +80,42 @@ def test_load(sdk: SDK, size_kb: int, rate: int):
     Load the system
     
     """
+    progress = rich.progress.Progress()
+    progress.start()
+    prog_bar = progress.add_task("Load", total=None)
+    sdk.setup(size_kb)
     while True:
         fn = lambda: random.choice([sdk.uload, sdk.dload])(size_kb)
         start_time = time.time_ns()
         fn()
+        progress.update(prog_bar, advance=1)
         end_time = time.time_ns()
         duration = (end_time - start_time) / 1e9
         time.sleep(max(0, 1/rate - duration))
+    sdk.teardown()
 
 if __name__ == "__main__":
-
-    linspace = [2**i for i in range(16)]
+    
     parser = argparse.ArgumentParser()
-
+    
     # args for connection
     parser.add_argument("--conn", type=str, choices=["agent", "ssh"], required=True, help="Connection Type")
     parser.add_argument("--api", type=str, help="[Agent] API URL")
     parser.add_argument("--agent", type=str, help="[Agent] Agent ID")
     parser.add_argument("--proxy", type=str, help="[SSH] Proxy Addr (user@hostname)")
     parser.add_argument("--remote", type=str, help="[SSH] Remote Addr (user@hostname)")
-
+    
     # args for task
     parser.add_argument("--task", type=str, choices=["bench", "load"], required=True, help="Task to perform")
     parser.add_argument("--size", type=int, default=32, help="[Bench] Payload size (KB)")
     parser.add_argument("--reps", type=int, default=100, help="[Bench] Number of repetitions")
     parser.add_argument("--rate", type=int, default=1, help="[Load] Request rate (req/s)")
-
+    
+    # parse args
     args = parser.parse_args()
     sdk: SDK
-
+    
+    # logic
     if args.conn == "agent":
         assert args.api is not None
         assert args.agent is not None
@@ -107,18 +126,29 @@ if __name__ == "__main__":
         sdk = SSHSDK(args.proxy, args.remote)
     else:
         raise ValueError("Invalid connection type. Choose 'agent' or 'ssh'.")
-
-    sdk.setup(linspace)
+    
     if args.task == "bench":
-        latency_ul = test_latency(sdk, "uload", args.size, args.reps)
-        print(f"Latency (U): {latency_ul:.6f} s")
-        latency_dl = test_latency(sdk, "dload", args.size, args.reps)
-        print(f"Latency (D): {latency_dl:.6f} s")
-        thruput_ul = test_thruput(sdk, "uload", args.size, args.reps)
-        print(f"Thruput (U): {thruput_ul:.6f} MB/s")
-        thruput_dl = test_thruput(sdk, "dload", args.size, args.reps)
-        print(f"Thruput (D): {thruput_dl:.6f} MB/s")
+        # run tests
+        print("Running Benchmarks...")
+        latency_ul = test_latency(sdk, "UL", args.size, args.reps)
+        latency_dl = test_latency(sdk, "DL", args.size, args.reps)
+        thruput_ul = test_thruput(sdk, "UL", args.size, args.reps)
+        thruput_dl = test_thruput(sdk, "DL", args.size, args.reps)
+        # save summary
+        result = dict(
+            size_kb=args.size,
+            latency_ul=latency_ul,
+            latency_dl=latency_dl,
+            thruput_ul=thruput_ul,
+            thruput_dl=thruput_dl
+        )
+        line = json.dumps(result)
+        with open("results.jsonl", "a") as f:
+            f.write(line + "\n")
+        print("latency_ul,latency_dl,thruput_ul,thruput_dl")
+        print(f"{latency_ul:.6f},{latency_dl:.6f},{thruput_ul:.6f},{thruput_dl:.6f}")
     elif args.task == "load":
+        print("Creating Load...")
         test_load(sdk, args.size, args.rate)
     else:
         raise ValueError("Invalid task. Choose 'bench' or 'load'.")
--- sdk.py
+++ sdk.py
@@ -6,7 +6,7 @@ import paramiko
 class TestCase(ABC):
 
   @abstractmethod
-  def setup(self, linspace: list[int]) -> None: ...
+  def setup(self, size_kb: int) -> None: ...
 
   @abstractmethod
   def teardown(self) -> None: ...
@@ -44,24 +44,21 @@ class AgentSDK(SDK):
 
   api_url: str
   agent_id: str
-  linspace: list[int]
 
   def __init__(self, api_url: str, agent_id: str) -> None:
     self.api_url = api_url
     self.agent_id = agent_id
 
-  def setup(self, linspace: list[int]) -> None:
-    self.linspace = linspace
+  def setup(self, size_kb: int) -> None:
+    pass
 
   def teardown(self) -> None:
     pass
 
   def uload(self, size_kb: int) -> bytes:
-    assert size_kb in self.linspace
     return b""
 
   def dload(self, size_kb: int) -> bytes:
-    assert size_kb in self.linspace
     return b""
 
   def exec(self, cmd: str, input: bytes) -> tuple[bytes, bytes]:
@@ -88,7 +85,6 @@ class SSHSDK(SDK):
   proxy_host: str
   remote_uname: str
   remote_host: str
-  linspace: list[int]
   proxy_client: paramiko.SSHClient
   proxy_transp: paramiko.Transport
   bridge_channel: paramiko.Channel
@@ -98,13 +94,10 @@ class SSHSDK(SDK):
     (self.proxy_uname, self.proxy_host) = proxy_addr.split("@")
     (self.remote_uname, self.remote_host) = remote_addr.split("@")
 
-  def setup(self, linspace: list[int]) -> None:
-    self.linspace = linspace
-
+  def setup(self, size_kb: int) -> None:
     # Connect to proxy
     self.proxy_client = paramiko.SSHClient()
     self.proxy_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print(f"[SSH] {self.proxy_uname}@{self.proxy_host}")
     self.proxy_client.connect(username=self.proxy_uname, hostname=self.proxy_host)
 
     # Create a bridge channel from proxy to remote
@@ -116,13 +109,10 @@ class SSHSDK(SDK):
     # Connect to remote through tunnel
     self.remote_client = paramiko.SSHClient()
     self.remote_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print(f"[SSH] {self.remote_uname}@{self.remote_host}")
     self.remote_client.connect(username=self.remote_uname, hostname=self.remote_host, sock=self.bridge_channel)
 
-    # create files (bytes, chars)
-    for size_kb in self.linspace:
-      (stdin, stdout, stderr) = self.remote_client.exec_command(f"head -c {size_kb * 1024} < /dev/urandom > bytes_{size_kb}k")
-      (stdin, stdout, stderr) = self.remote_client.exec_command(f"tr -dc 'A-Za-z0-9' < /dev/urandom | head -c {size_kb * 1024} > chars_{size_kb}k")
+    # create data file
+    (stdin, stdout, stderr) = self.remote_client.exec_command(f"head -c {size_kb * 1024} < /dev/urandom > data_{size_kb}k")
 
   def teardown(self) -> None:
     self.remote_client.close()
@@ -130,15 +120,13 @@ class SSHSDK(SDK):
     self.proxy_client.close()
 
   def uload(self, size_kb: int) -> None:
-    assert size_kb in self.linspace
     data = os.urandom(size_kb * 1024)
-    stdin, stdout, stderr = self.remote_client.exec_command(f"cat > {size_kb}k")
+    stdin, stdout, stderr = self.remote_client.exec_command(f"cat > data_{size_kb}k")
     stdin.write(data)
     stdin.flush()
 
   def dload(self, size_kb: int) -> bytes:
-    assert size_kb in self.linspace
-    stdin, stdout, stderr = self.remote_client.exec_command(f"cat {size_kb}k")
+    stdin, stdout, stderr = self.remote_client.exec_command(f"cat data_{size_kb}k")
     data = stdout.read()
     return data
 
